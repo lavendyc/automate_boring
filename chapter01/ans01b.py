95deffac7bbdf36db0e5217814573416ae477140
lavendyc <bulu2@qq.com>
2017-05-27 01:36:01 +0100

    ans01.b

---
+++ chapter01/ans01b.py
@@ -0,0 +1,10 @@
+print('Enter the value of N:')
+n=str(input())
+print('Enter the value of L:')
+l=int(input())
+if(n<0):
+    print('ERROR')
+elif(l>=len(n)):
+    z='%0'+str(l)+'d'
+    s=z%(int(n))
+    print(s)
