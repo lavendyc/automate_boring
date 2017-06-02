8bb06e5bfb3e170d8e68de92c9230961a286faa1
lavendyc <bulu2@qq.com>
2017-06-02 01:23:22 +0100

    Practice question

---
+++ chapter02/C2 practice Q.py
@@ -0,0 +1,73 @@
+Q1,True & False
+Q2, and or not
+Q3.
+1. and
+True and True --- True
+True and False --- False
+False and False --- False
+2. or
+True or True --- True
+True or False --- True
+False or False --- False
+3. not
+not True --- False
+not False --- True
+
+Q4.
+False
+False
+True
+False
+False
+True
+
+Q5.
+==, !=, < > <= >=
+
+Q6
+== equal to
+= storing a value in a variable
+
+Q7
+Condition is an expression used in a flow control statment and evaluate a Boolean value.
+
+
+Q9
+
+if spam==1:
+    print ('Hello')
+elif spam == 2:
+    print ('Howdy')
+else:
+    print('Greetings!')
+
+Q10
+Crtl C
+
+Q11
+
+break: break out of a loop's clause early and execute the next code
+continue: jump back to the start of the loop
+
+Q12
+range(10)
+ranges 0-10
+0 1 2 3 4 5 6 7 8 9 
+range(0,10)
+tell the loop start from 0 to 9
+0 1 2 3 4 5 6 7 8 9
+range(0,10,1)
+tell the loop to increase the variable by 1 until 10
+0 1 2 3 4 5 6 7 8 9 10
+Q13
+1.
+for i in range (1,11):
+    print(i)
+2.
+i=1
+while i <=10:
+    i=i+1
+    print(i)
+
+Q14
+spam,bacon(£©
