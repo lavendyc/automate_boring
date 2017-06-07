5807a70995344774fa44823846050833b42eefc7
lavendyc <bulu2@qq.com>
2017-06-07 01:40:05 +0100

    Update Q2.py

--- chapter02/Q2.py
+++ chapter02/Q2.py
@@ -1,8 +1,18 @@
 print('Please enter the value of N:')
-n=str(input())
-while int(n)<1 and int(n)>1000 and not n.isdigit():
+n=input()
+x=1
+while x == False:
     print('N should be a number between 1 and 1000, please try again.')
-    n=str(input())
+    n = str(input())
+    if not isinstance(n,int):
+        x = False
+    elif int(n)<1 or int(n)>1000:
+        x = False
+    else:
+        x = True
+        if x == True:
+            break
+    
 if int(n)>=1 and int(n)<=1000:
     i=1
     for s in range(0,int(n)):
@@ -14,4 +24,5 @@ if int(n)>=1 and int(n)<=1000:
                     result = False
             if result == True:
                 print(str(i)+','),
-                break    
+                break
+
