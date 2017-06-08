a5496514c9a8f7668f9dfc5196b5f62523f5829d
lavendyc <bulu2@qq.com>
2017-06-08 02:09:43 +0100

    Create Q2.py

--- chapter02/Q2.py
+++ chapter02/Q2.py
@@ -1,28 +1,27 @@
 print('Please enter the value of N:')
-n=input()
-x=1
-while x == False:
-    print('N should be a number between 1 and 1000, please try again.')
-    n = str(input())
-    if not isinstance(n,int):
-        x = False
+n = None
+while True:
+    if n != None:
+        print 'Please try again'
+    n = str(raw_input())
+    if not n.isdigit():
+        continue
     elif int(n)<1 or int(n)>1000:
-        x = False
-    else:
-        x = True
-        if x == True:
-            break
-    
+        continue
+    break
+
 if int(n)>=1 and int(n)<=1000:
     i=1
     for s in range(0,int(n)):
-        while i<10000:
+        while True:
             result = True
             i+=1
             for j in range(2,int(i**0.5)+1):
                 if int(i)%j == 0:
                     result = False
             if result == True:
-                print(str(i)+','),
+                if s == int(n)-1:
+                    print str(i),
+                else:
+                    print(str(i)+','),
                 break
-
